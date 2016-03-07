cee8b3db23056b63348171664e3e666a504ea780
matttung <matttung77@hotmail.com>
2016-03-07 23:40:40 +0800

    Blank Project

---
+++ MVC5HomeWorkW1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5HomeWorkW1.MvcApplication" Language="C#" %>
---
+++ MVC5HomeWorkW1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5HomeWorkW1.Startup))]
+namespace MVC5HomeWorkW1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
